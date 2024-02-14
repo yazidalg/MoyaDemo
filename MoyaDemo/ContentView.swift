c23eabcb700aa68fe0db066f32b5059d5b69f3a6
Yazid Al Ghozali <yazid.al2418@gmail.com>
2024-02-14 18:31:13 +0700

    Initial Commit

---
+++ MoyaDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoyaDemo
+//
+//  Created by Yazid Al Ghozali on 14/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoyaDemo/MoyaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoyaDemoApp.swift
+//  MoyaDemo
+//
+//  Created by Yazid Al Ghozali on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoyaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
